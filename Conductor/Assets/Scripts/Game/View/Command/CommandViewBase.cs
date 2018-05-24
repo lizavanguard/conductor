9a0a4437e4bcad5c9d385e17aef68dcfa1450e61
MasahiroNakajima1202 <MasahiroNakajima1202@gmail.com>
2018-05-25 01:30:30 +0900

    View, Modelそれぞれのコマンドクラスの基底を作成

---
+++ Conductor/Assets/Scripts/Game/View/Command/CommandViewBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Consuctor.Game.View
+{
+    public abstract class CommandViewBase
+    {
+        public CommandViewBase()
+        { }
+
+        /// <summary>
+        /// Updateの中で一回ずつ呼ぶ
+        /// 1回で完了するコマンドの場合はHasFinishedで即時trueを返す
+        /// </summary>
+        public virtual void Run()
+        { }
+
+        /// <summary>
+        /// Runを呼んだ直後にこの関数で終了判定を取り、終了していたらコマンドを破棄する
+        /// </summary>
+        /// <returns></returns>
+        public virtual bool HasFinished()
+        {
+            return true;
+        }
+    }
+}
