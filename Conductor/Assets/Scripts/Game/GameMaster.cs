15e4e5a4168f111061aa011a261bbf11bebd941a
MasahiroNakajima1202 <MasahiroNakajima1202@gmail.com>
2018-05-30 02:02:29 +0900

    GameManagerでMessageBusのインスタンス生成

--- Conductor/Assets/Scripts/Game/GameMaster.cs
+++ Conductor/Assets/Scripts/Game/GameMaster.cs
@@ -6,12 +6,15 @@ namespace Consuctor.Game
 {
     public class GameMaster : MonoBehaviour
     {
+        // 生成したほとんどのインスタンスにこれを食わせる
+        MessageBus.Manager messageBusManager;
+
         /// <summary>
         /// 各Modelの生成と初期化
         /// </summary>
         private void Awake()
         {
-            
+            messageBusManager = new MessageBus.Manager();
         }
 
         /// <summary>
