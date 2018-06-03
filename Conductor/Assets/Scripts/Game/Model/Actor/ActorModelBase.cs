ce6c6f9c34376a559c9867d8e3654bab6dcd7c90
MasahiroNakajima1202 <MasahiroNakajima1202@gmail.com>
2018-06-03 23:25:22 +0900

    ActorModelBase作成

---
+++ Conductor/Assets/Scripts/Game/Model/Actor/ActorModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Conductor.Game.Model
+{
+    public abstract class ActorModelBase
+    {
+        // FIXME: ActorViewBaseができたらその参照を持つようにする
+
+        public ActorModelBase()
+        { }
+
+        public abstract void Update();
+    }
+}
