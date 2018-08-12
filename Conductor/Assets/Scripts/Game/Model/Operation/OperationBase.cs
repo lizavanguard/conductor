4abd80f464cc19b5ec06aa1f5cef64a181694131
MasahiroNakajima1202 <MasahiroNakajima1202@gmail.com>
2018-08-12 21:34:35 +0900

    operationの基底クラス作成

---
+++ Conductor/Assets/Scripts/Game/Model/Operation/OperationBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Conductor.Game.Model
+{
+    public abstract class OperationBase
+    {
+        ActorModelBase owner;
+        public OperationBase(ActorModelBase owner)
+        {
+            this.owner = owner;
+        }
+    }
+}
