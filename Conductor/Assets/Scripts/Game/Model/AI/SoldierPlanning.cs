72f422632be525e9dbb1dd64f2a73531ebbc2145
MasahiroNakajima1202 <MasahiroNakajima1202@gmail.com>
2018-08-14 01:27:42 +0900

    作業メモ

--- Conductor/Assets/Scripts/Game/Model/AI/SoldierPlanning.cs
+++ Conductor/Assets/Scripts/Game/Model/AI/SoldierPlanning.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 namespace Conductor.Game.Model
 {
+    // モックとして「プレイヤーの方向を向く」と「プレイヤーを攻撃する」を実装する
+    // 実際にはこれらのどっちを選ぶかは隊長クラスのplanningから判断される
     public class SoldierPlanning
     {
         public SoldierPlanning()
