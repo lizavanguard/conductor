5dbe8974a2c187ca8e1c43fe35ffd74c8b694e8c
MasahiroNakajima1202 <MasahiroNakajima1202@gmail.com>
2018-08-14 01:22:41 +0900

    空のNavigationとPlanningを作成

---
+++ Conductor/Assets/Scripts/Game/Model/AI/SoldierPlanning.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Conductor.Game.Model
+{
+    public class SoldierPlanning
+    {
+        public SoldierPlanning()
+        {
+        }
+    }
+}
