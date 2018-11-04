12554ae71b4bb230f795ee1eabe3770146cc5807
MasahiroNakajima1202 <MasahiroNakajima1202@gmail.com>
2018-11-04 13:33:44 +0900

    編集メソッド定義

--- Conductor/Assets/Scripts/Game/Model/AI/SoldierPlanning.cs
+++ Conductor/Assets/Scripts/Game/Model/AI/SoldierPlanning.cs
@@ -53,26 +53,40 @@ namespace Conductor.Game.Model
             // 該当bitがtrue => その条件は成立している
             // 該当bitがfalse => その条件の成否は不明/不問
             // 明示的に不成立の条件を入れる時は、専用のbitを作る
-            uint firstConditionFlag;
-            public uint FirstConditionFlag { get { return firstConditionFlag; } }
+            uint conditionFlag;
+            public uint ConditionFlag { get { return conditionFlag; } }
 
             public Condition(ConditionType[] trueList)
             {
-                firstConditionFlag = 0;
+                conditionFlag = 0;
                 foreach (var type in trueList)
                 {
-                    firstConditionFlag |= 1U << (int)type;
+                    conditionFlag |= 1U << (int)type;
                 }
             }
 
             public bool Satisfy(Condition other)
             {
-                return other.firstConditionFlag == (firstConditionFlag & other.firstConditionFlag);
+                return other.conditionFlag == (conditionFlag & other.conditionFlag);
             }
 
             public bool IsSatisfiedBy(Condition other)
             {
                 return other.Satisfy(this);
+            }
+
+            public void SetFlag(ConditionType type, bool flag)
+            {
+                var mask = 1U << (int)type;
+
+                if (flag)
+                {
+                    conditionFlag |= mask;
+                }
+                else
+                {
+                    conditionFlag &= ~mask;
+                }
             }
         }
 
@@ -84,6 +98,8 @@ namespace Conductor.Game.Model
 
         // 現在構築中のプランニング
         List<Node> currentPlanningChain;
+
+        Condition currentCondition;
 
         ActorModelBase owner;
 
@@ -100,6 +116,8 @@ namespace Conductor.Game.Model
             // 8: OperationTypeを増築
 
             nodeList = GenerateOperationNodes(owner, commandRunner, gameMaster);
+
+            currentCondition = new Condition(new ConditionType[] { });
         }
 
         public void SetGoal(Condition goal)
@@ -108,6 +126,8 @@ namespace Conductor.Game.Model
         }
 
         // FIXME: 本当は外部ファイルから読み込むべき
+        // 最終的には敵タイプとか味方ユニットごとに外部データから読み込み
+        // 味方の方はユーザーがいじれるようにもしたい
         Node[] GenerateOperationNodes(ActorModelBase owner, CommandRunner commandRunner, GameMaster gameMaster)
         {
             List<Node> newNodeList = new List<Node>();
