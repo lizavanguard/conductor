98c26ec99be3184b4960d63af07605ea816d72ac
MasahiroNakajima1202 <MasahiroNakajima1202@gmail.com>
2018-11-26 23:53:30 +0900

    add actor updater

---
+++ Conductor/Assets/Scripts/Game/ActorUpdater.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Conductor.Game.Model;
+using UnityEngine;
+
+namespace Conductor.Game
+{
+    public class ActorUpdater
+    {
+        List<ActorModelBase> friends;
+        List<ActorModelBase> enemies;
+        public ActorUpdater(View.ActorPrefabReference prefabReference)
+        {
+            ActorFactory factory = new ActorFactory(prefabReference);
+        }
+    }
+}
