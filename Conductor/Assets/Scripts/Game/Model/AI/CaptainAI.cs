3c08c4c400521cbc6c3fedf154e497ffd19565be
MasahiroNakajima1202 <MasahiroNakajima1202@gmail.com>
2018-12-09 18:20:24 +0900

    rename

--- Conductor/Assets/Scripts/Game/Model/AI/CaptainAI.cs
+++ Conductor/Assets/Scripts/Game/Model/AI/CaptainAI.cs
@@ -6,11 +6,10 @@ namespace Conductor.Game.Model
 {
     /// <summary>
     /// 部隊長としての振る舞いを記述するクラス
-    /// 名前的にMonoBehaviourっぽいがそうではなく、ActorModelBaseと違って兵士そのものを示すわけではないという命名
     /// </summary>
-    public class CaptainBehaviour
+    public class CaptainAI
     {
-        public CaptainBehaviour()
+        public CaptainAI()
         { }
     }
 }
