62e65de5ddbbdf2f88dc7f716496db479105d796
MasahiroNakajima1202 <MasahiroNakajima1202@gmail.com>
2018-12-09 19:11:28 +0900

    rename node class

---
+++ Conductor/Assets/Scripts/Game/Model/AI/PlanningNode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Conductor.Game.Model
+{
+    // FIXME: なんとかこれをデータ化したい
+    // planningChainを作るときはafterとbeforeで変化のあったbitに着目して次の状況を作る
+    class PlannngNode
+    {
+        // 前提条件
+        Condition beforeCondition;
+
+        // 目標条件
+        Condition afterCondition;
+
+        // 行動
+        OperationType operationType;
+
+        public Condition BeforeCondition { get { return beforeCondition; } }
+        public Condition AfterCondition { get { return afterCondition; } }
+        public OperationType OperationType { get { return operationType; } }
+
+
+        public PlannngNode(ActorModelBase owner, CommandRunner commandRunner, GameMaster gameMaster, Condition before, Condition after, OperationType operationType)
+        {
+            this.beforeCondition = before;
+            this.afterCondition = after;
+            this.operationType = operationType;
+        }
+    }
+}
--- Conductor/Assets/Scripts/Game/Model/AI/SoldierPlanning.cs
+++ Conductor/Assets/Scripts/Game/Model/AI/SoldierPlanning.cs
@@ -9,40 +9,14 @@ namespace Conductor.Game.Model
     // 実際にはこれらのどっちを選ぶかは隊長クラスのplanningから判断される
     public class SoldierPlanning
     {
-        // FIXME: なんとかこれをデータ化したい
-        // planningChainを作るときはafterとbeforeで変化のあったbitに着目して次の状況を作る
-        class Node
-        {
-            // 前提条件
-            Condition beforeCondition;
-
-            // 目標条件
-            Condition afterCondition;
-
-            // 行動
-            OperationType operationType;
-
-            public Condition BeforeCondition { get { return beforeCondition; } }
-            public Condition AfterCondition { get { return afterCondition; } }
-            public OperationType OperationType { get { return operationType; } }
-
-
-            public Node(ActorModelBase owner, CommandRunner commandRunner, GameMaster gameMaster, Condition before, Condition after, OperationType operationType)
-            {
-                this.beforeCondition = before;
-                this.afterCondition = after;
-                this.operationType = operationType;
-            }
-        }
-
         // 候補のNode
-        Node[] baseNodeList;
+        PlannngNode[] baseNodeList;
 
         // プランニングの最終目標
         Condition goalCondition;
 
         // 現在構築済のプランニング
-        List<Node> currentPlanningChain;
+        List<PlannngNode> currentPlanningChain;
 
         Condition currentCondition;
 
@@ -60,7 +34,7 @@ namespace Conductor.Game.Model
             baseNodeList = GenerateOperationNodes(owner, commandRunner, gameMaster);
 
             currentCondition = new Condition(new ConditionType[] { });
-            currentPlanningChain = new List<Node>();
+            currentPlanningChain = new List<PlannngNode>();
 
             this.owner = owner;
             this.commandRunner = commandRunner;
@@ -136,7 +110,7 @@ namespace Conductor.Game.Model
             return currentPlanningChain != null && currentPlanningChain.Count > 0;
         }
 
-        List<Node> ChainNode(List<Node> chain, Condition goal)
+        List<PlannngNode> ChainNode(List<PlannngNode> chain, Condition goal)
         {
             // currentStateがゴールを満たしているかどうかチェック(終了判定)
             if (currentCondition.Satisfy(goal))
@@ -145,7 +119,7 @@ namespace Conductor.Game.Model
             }
 
             // 現在ゴールを満たすafterConditionを持ったNodeを一覧化
-            var newNodeList = new List<Node>();
+            var newNodeList = new List<PlannngNode>();
             foreach (var node in baseNodeList)
             {
                 if (node.AfterCondition.Satisfy(goal))
@@ -169,7 +143,7 @@ namespace Conductor.Game.Model
                 }
 
                 // よしなに(最初は順番に)選ぶ 現在の状態に最も近付くようなやつがいい
-                var tempChain = new List<Node>(chain);
+                var tempChain = new List<PlannngNode>(chain);
                 tempChain.Add(newNode);
 
                 // 現在ゴールを更新
@@ -192,9 +166,9 @@ namespace Conductor.Game.Model
         // FIXME: 本当は外部ファイルから読み込むべき
         // 最終的には敵タイプとか味方ユニットごとに外部データから読み込み
         // 味方の方はユーザーがいじれるようにもしたい
-        Node[] GenerateOperationNodes(ActorModelBase owner, CommandRunner commandRunner, GameMaster gameMaster)
+        PlannngNode[] GenerateOperationNodes(ActorModelBase owner, CommandRunner commandRunner, GameMaster gameMaster)
         {
-            List<Node> newNodeList = new List<Node>();
+            List<PlannngNode> newNodeList = new List<PlannngNode>();
 
             // 最も近くにいる敵対陣営のキャラのほうを向く
             {
@@ -205,7 +179,7 @@ namespace Conductor.Game.Model
                 {
                     ConditionType.LookToSomeEnemy,
                 };
-                var node = new Node(owner, commandRunner, gameMaster, new Condition(beforeList), new Condition(afterList), OperationType.LookToNearestEnemy);
+                var node = new PlannngNode(owner, commandRunner, gameMaster, new Condition(beforeList), new Condition(afterList), OperationType.LookToNearestEnemy);
                 newNodeList.Add(node);
             }
 
@@ -219,7 +193,7 @@ namespace Conductor.Game.Model
                 {
                     ConditionType.HittingSomeEnemy,
                 };
-                var node = new Node(owner, commandRunner, gameMaster, new Condition(beforeList), new Condition(afterList), OperationType.AttackNearestEnemy);
+                var node = new PlannngNode(owner, commandRunner, gameMaster, new Condition(beforeList), new Condition(afterList), OperationType.AttackNearestEnemy);
                 newNodeList.Add(node);
             }
 
@@ -232,7 +206,7 @@ namespace Conductor.Game.Model
                 {
                     ConditionType.CanTargetSomeEnemy,
                 };
-                var node = new Node(owner, commandRunner, gameMaster, new Condition(beforeList), new Condition(afterList), OperationType.SearchEnemy);
+                var node = new PlannngNode(owner, commandRunner, gameMaster, new Condition(beforeList), new Condition(afterList), OperationType.SearchEnemy);
                 newNodeList.Add(node);
             }
 
