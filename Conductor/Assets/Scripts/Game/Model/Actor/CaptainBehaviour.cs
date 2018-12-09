fb755029b4017958c28723996a126e13d4596879
MasahiroNakajima1202 <MasahiroNakajima1202@gmail.com>
2018-12-09 17:53:05 +0900

    とりあえずクラス作成

---
+++ Conductor/Assets/Scripts/Game/Model/Actor/CaptainBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Conductor.Game.Model
+{
+    /// <summary>
+    /// 部隊長としての振る舞いを記述するクラス
+    /// 名前的にMonoBehaviourっぽいがそうではなく、ActorModelBaseと違って兵士そのものを示すわけではないという命名
+    /// </summary>
+    public class CaptainBehaviour
+    {
+        public CaptainBehaviour()
+        { }
+    }
+}
