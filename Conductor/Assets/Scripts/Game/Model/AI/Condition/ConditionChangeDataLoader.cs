89742d40573b130bee2b8c8a08008312a1ac15ce
MasahiroNakajima1202 <MasahiroNakajima1202@gmail.com>
2019-05-08 22:35:42 +0900

    create empty loader class

---
+++ Conductor/Assets/Scripts/Game/Model/AI/Condition/ConditionChangeDataLoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Conductor.Game.Model
+{
+    public class OperationChangeDataLoader
+    {
+        public OperationChangeDataLoader()
+        {
+        }
+    }
+}
