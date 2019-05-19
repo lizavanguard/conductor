94815b515435ce81523f16add19521856d7c0f60
MasahiroNakajima1202 <MasahiroNakajima1202@gmail.com>
2019-05-19 22:24:53 +0900

    create empty class

---
+++ Conductor/Assets/Scripts/Game/Model/CursorModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Conductor.Game.View;
+
+namespace Conductor.Game.Model
+{
+    public class CursorModel
+    {
+        CursorView view;
+        ActorModelBase mainActor;
+        FieldModel field;
+        Vector3 position;
+        ActorUpdater actorUpdater;
+
+        public CursorModel(CursorView view, ActorModelBase mainActor, FieldModel field, ActorUpdater actorUpdater)
+        {
+            this.view = view;
+            this.mainActor = mainActor;
+            this.field = field;
+            this.actorUpdater = actorUpdater;
+        }
+
+        public void Update()
+        {
+            // 位置更新
+
+            // 近くにいるactorをさがす
+
+            // 近くにいたらそいつを指すようにViewに位置を与える
+
+
+        }
+    }
+}
